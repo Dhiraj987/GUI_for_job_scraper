e81d8ad9c96c044642eaea3c87eab3686a8b49f5
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-06-26 17:46:52 -0400

    done with layouts

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import tkinter as tk
 import tkinter.font as fnt
 
-
 HEIGHT = 500
 WIDTH = 800
 
 root = tk.Tk()
+root.title("Job Parsing Portal")
 
 canvas = tk.Canvas(root, height = HEIGHT, width= WIDTH)
 canvas.pack()
@@ -14,25 +14,25 @@ background_image = tk.PhotoImage(file='bg-image.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-label = tk.Label(root, text = "Scrape job Postings", bg = 'white', font = fnt.Font(size = 17))
-label.place(relx=0.5, rely=0.05,  relheight= 0.1, relwidth=0.25, anchor='n')
+label = tk.Label(root, text = "Scrape job Postings", bg = '#f2fcbd', font = fnt.Font(size = 20))
+label.place(relx=0.5, rely=0.025,  relheight= 0.15, relwidth=0.30, anchor='n')
 
 frame = tk.Frame(root, bg = '#b0d9b3', bd=5)
 frame.place(relx = 0.5, rely = 0.2, relwidth=0.75, relheight= 0.3, anchor='n')
 
-entry = tk.Entry(frame, font=40)
-entry.place(rely = 0.05, relwidth=0.65, relheight=0.4)
+entry = tk.Entry(frame, bg = '#fcffeb', font = fnt.Font(size = 20))
+entry.place(relx = 0.025, rely = 0.05, relwidth=0.65, relheight=0.4)
 
-entry2 = tk.Entry(frame, font = 40)
-entry2.place(rely = 0.55, relwidth= 0.65, relheight= 0.4)
+entry2 = tk.Entry(frame, bg = '#fcffeb', font = fnt.Font(size = 20))
+entry2.place(relx = 0.025, rely = 0.55, relwidth= 0.65, relheight= 0.4)
 
-button = tk.Button(frame, text = "Find Jobs", font = fnt.Font(size = 20))
-button.place(relx = 0.68, rely = 0.25, relheight=0.5, relwidth = 0.3)
+button = tk.Button(frame, text = "Find Jobs", fg ='#3bb1cc', font = fnt.Font(size = 22))
+button.place(relx = 0.7, rely = 0.25, relheight=0.45, relwidth = 0.27)
 
 lower_frame = tk.Frame(root, bg = '#b0d9b3', bd = 10)
 lower_frame.place(relx = 0.5, rely = 0.55, relheight=0.35, relwidth= 0.75, anchor='n')
 
-lower_label = tk.Label(lower_frame)
+lower_label = tk.Label(lower_frame, bg = '#fcffeb')
 lower_label.place(relx = 0.5, rely = 0.02, relwidth= 0.95, relheight= 0.95, anchor ='n')
 
 root.mainloop()
