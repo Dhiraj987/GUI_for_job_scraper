5cc3a8d3c35271fcc45b931d174eba2797dea10f
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-06-26 17:26:28 -0400

    bg

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.font as fnt
+
 
 HEIGHT = 500
 WIDTH = 800
@@ -8,10 +10,14 @@ root = tk.Tk()
 canvas = tk.Canvas(root, height = HEIGHT, width= WIDTH)
 canvas.pack()
 
-label = tk.Label(root, text = "Scrape job Postings", bg = 'white')
+background_image = tk.PhotoImage(file='bg-image.png')
+background_label = tk.Label(root, image=background_image)
+background_label.place(relwidth=1, relheight=1)
+
+label = tk.Label(root, text = "Scrape job Postings", bg = 'white', font = fnt.Font(size = 17))
 label.place(relx=0.5, rely=0.05,  relheight= 0.1, relwidth=0.25, anchor='n')
 
-frame = tk.Frame(root, bg = '#80c1ff', bd=5)
+frame = tk.Frame(root, bg = '#b0d9b3', bd=5)
 frame.place(relx = 0.5, rely = 0.2, relwidth=0.75, relheight= 0.3, anchor='n')
 
 entry = tk.Entry(frame, font=40)
@@ -20,10 +26,10 @@ entry.place(rely = 0.05, relwidth=0.65, relheight=0.4)
 entry2 = tk.Entry(frame, font = 40)
 entry2.place(rely = 0.55, relwidth= 0.65, relheight= 0.4)
 
-button = tk.Button(frame, text = "Test Button", font = 40)
+button = tk.Button(frame, text = "Find Jobs", font = fnt.Font(size = 20))
 button.place(relx = 0.68, rely = 0.25, relheight=0.5, relwidth = 0.3)
 
-lower_frame = tk.Frame(root, bg = '#80c1ff', bd = 10)
+lower_frame = tk.Frame(root, bg = '#b0d9b3', bd = 10)
 lower_frame.place(relx = 0.5, rely = 0.55, relheight=0.35, relwidth= 0.75, anchor='n')
 
 lower_label = tk.Label(lower_frame)
