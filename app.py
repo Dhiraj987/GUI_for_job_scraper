1dc0ce74c17005b0e612d77ef48c074489c6a020
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-06-27 17:19:59 -0400

    minor changes

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
 import tkinter as tk
 import tkinter.font as fnt
 from tkinter import END
-from scrapers import linkedIn_scraper
+from scrapers import for_both
 
 
 
 def get_jobs( job, loc):
-    lower_label.config(text = "Parsing Jobs")
-    linkedIn_scraper.run(job, loc)
+    lower_label.config(text = "Parsing Job Postings")
+    return for_both.run(job, loc)
 
 root = tk.Tk()
 root.title("Job Parsing Portal")
@@ -19,7 +19,7 @@ background_image = tk.PhotoImage(file='bg-image.png')
 background_label = tk.Label(root, image=background_image)
 background_label.place(relwidth=1, relheight=1)
 
-label = tk.Label(root, text = "Scrape job Postings", bg = '#f2fcbd', font = fnt.Font(size = 25))
+label = tk.Label(root, text = "Scrape job Postings", fg = '#3bb1cc', font = fnt.Font(size = 25))
 label.place(relx=0.5, rely=0.025,  relheight= 0.15, relwidth=0.30, anchor='n')
 
 frame = tk.Frame(root, bg = '#b0d9b3', bd=5)
@@ -31,7 +31,7 @@ entry.place(relx = 0.025, rely = 0.05, relwidth=0.65, relheight=0.4)
 entry2 = tk.Entry(frame, bg = '#fcffeb', font = fnt.Font(size = 20), bd=2)
 entry2.place(relx = 0.025, rely = 0.55, relwidth= 0.65, relheight= 0.4)
 
-button = tk.Button(frame, text = "Find Jobs", fg ='#3bb1cc', font = fnt.Font(size = 22), command = lambda: get_jobs(entry.get(),entry2.get()))
+button = tk.Button(frame, text = "Find Jobs", fg ='black', font = fnt.Font(size = 22), command = lambda: get_jobs(entry.get(),entry2.get()))
 button.place(relx = 0.7, rely = 0.25, relheight=0.45, relwidth = 0.27)
 
 lower_frame = tk.Frame(root, bg = '#b0d9b3', bd = 10)
