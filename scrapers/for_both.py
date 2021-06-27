27f6a65677c97579f25e233e04e1c23657d6c23b
Dhiraj Kumar Chaudhary <dhirajc963@gmail.com>
2021-06-27 17:21:19 -0400

    modified to parse from indeed and linkedIn

--- scrapers/for_both.py
+++ scrapers/for_both.py
@@ -1,10 +1,12 @@
 from scrapers import indeed_scraper 
 from scrapers import linkedIn_scraper
 
-def run():
-    print('\n')
-    job = input("Enter the job: ")
-    location = input("Enter the location: ")
+def run(job, location):
+    if job == None:
+        print('\n')
+        job = input("Enter the job: ")
+    if location == None:
+        location = input("Enter the location: ")
     print("\n\tWorking with Indeed scraper")
     indeed_scraper.run(job, location)
     print("\n\tWorking with LinkedIn scraper")
